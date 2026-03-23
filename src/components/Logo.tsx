5d722e933c29d57c3fef86d068aae98f2b2bbcd5
Himanshu Yadav <write.himanshu263@gmail.com>
2026-03-23 18:32:28 +0530

    first commit

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+export const PdfKnifeLogo = ({ size = 24, className = "" }: { size?: number, className?: string, iconColor?: string, partColor?: string }) => (
+  <img 
+    src="./icons/logo.png" 
+    alt="PDF Knife Logo"
+    width={size} 
+    height={size} 
+    className={className}
+    style={{ objectFit: 'contain' }}
+  />
+)
---
+++ src/utils/haptics.ts
@@ -0,0 +1,5 @@
+// Web-only: haptics are no-ops since there's no native haptic API on web
+
+export const hapticImpact = async () => {};
+export const hapticSuccess = async () => {};
+export const hapticSelection = async () => {};
