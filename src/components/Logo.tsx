2954e0164b20aafb5e2be63912270f85ba1f94ce
Himanshu Yadav <write.himanshu263@gmail.com>
2026-03-24 11:57:26 +0530

    Update logo references to assets/logo.png and consolidate

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 export const PdfKnifeLogo = ({ size = 113, className = "" }: { size?: number, className?: string, iconColor?: string, partColor?: string }) => (
   <img
-    src="./assets/logo2.png"
+    src="./assets/logo.png"
     alt="PDF Knife Logo"
     width={size}
     height={size}
