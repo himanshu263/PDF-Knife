e4eaacf1cd686e703e35ff3bec27d53eb33699c3
Himanshu Yadav <write.himanshu263@gmail.com>
2026-03-24 11:15:05 +0530

    d

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,9 @@
-export const PdfKnifeLogo = ({ size = 24, className = "" }: { size?: number, className?: string, iconColor?: string, partColor?: string }) => (
-  <img 
-    src="./icons/logo.jpg" 
+export const PdfKnifeLogo = ({ size = 44, className = "" }: { size?: number, className?: string, iconColor?: string, partColor?: string }) => (
+  <img
+    src="./assets/logo.jpg"
     alt="PDF Knife Logo"
-    width={size} 
-    height={size} 
+    width={size}
+    height={size}
     className={className}
     style={{ objectFit: 'contain' }}
   />
